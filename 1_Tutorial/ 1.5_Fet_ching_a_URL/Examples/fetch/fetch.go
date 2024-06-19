54622f952728699dbcbe3839c9b6d2edaaa2abd6
Platohykj <abc181891aa@outlook.com>
2024-06-20 04:33:02 +0800

    20/6/2024

--- 1_Tutorial/ 1.5_Fet_ching_a_URL/Examples/fetch/fetch.go
+++ 1_Tutorial/ 1.5_Fet_ching_a_URL/Examples/fetch/fetch.go
@@ -1,5 +1,26 @@
+// Fetch prints the content found at a URL.
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
 }
